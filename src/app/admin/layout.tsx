568c1a33aeec6098b2c50ea4da55c3bdbec9c105
codtech1208 <harishneela83@gmail.com>
2026-09-24 10:19:43 +0530

    fix(admin): remove scroll lock to allow full mobile scrolling on forms

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -18,12 +18,12 @@ export default async function AdminLayout({
   }
 
   return (
-    <div className="flex flex-col md:flex-row h-[100dvh] bg-gray-50">
+    <div className="flex flex-col md:flex-row min-h-screen bg-gray-50">
       <AdminSidebar userEmail={user.email || ''} />
 
       {/* Main Content */}
-      <main className="flex-1 flex flex-col overflow-hidden relative">
-        <div className="flex-1 overflow-y-auto bg-gray-50 p-4 md:p-8">
+      <main className="flex-1 flex flex-col w-full">
+        <div className="flex-1 bg-gray-50 p-4 md:p-8">
           {children}
         </div>
       </main>
